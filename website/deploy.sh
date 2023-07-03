76718ae2df111433d5315b53d89b8bf9f01a24a8
bubu <albertofdr@gmail.com>
2023-07-03 18:55:23 +0200

    Updated deploy

--- website/deploy.sh
+++ website/deploy.sh
@@ -3,4 +3,10 @@ hexo clean
 hexo generate 
 git add .
 git add ../docs
-git reset ../docs/index.html
+
+# ===== If there is not a new entry
+# git reset ../docs/index.html
+# 
+# ===== If there is a new entry
+# change the about w/o the last '/'
+# change the links of the list to add 'web-security-class/' before the path.
